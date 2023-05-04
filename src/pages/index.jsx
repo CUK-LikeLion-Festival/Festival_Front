c46c07e71bbfd8cea15668988dec162bc263f181
Seo Eun <seo606101@naver.com>
2023-05-04 20:40:33 +0900

    first init

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,32 @@
 import Layout from "../components/Layout/layout";
+import React from "react";
+import { SectionsContainer, Section } from "react-fullpage";
+
+const options = {
+  activeClass: "active", // the class that is appended to the sections links
+  anchors: [1, 2, 3], // the anchors for each sections
+  arrowNavigation: true, // use arrow keys
+  className: "SectionContainer", // the class name for the section container
+  delay: 1000, // the scroll animation speed
+  navigation: true, // use dots navigatio
+  scrollBar: false, // use the browser default scrollbar
+  sectionClassName: "Section", // the section class name
+  sectionPaddingTop: "0", // the section top padding
+  sectionPaddingBottom: "0", // the section bottom padding
+  verticalAlign: false, // align the content of each section vertical};
+  // => in the render() method of your app
+};
 
 function IndexPage() {
   return (
     <div>
-      <Layout></Layout>
+      <Layout>
+        <SectionsContainer {...options}>
+          <Section>Page 1</Section>
+          <Section>Page 2</Section>
+          <Section>Page 3</Section>
+        </SectionsContainer>
+      </Layout>
     </div>
   );
 }
