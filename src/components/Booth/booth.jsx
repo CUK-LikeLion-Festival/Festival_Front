7810f355cbfb888090a5bc50b3119b01af5db581
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-05-10 17:20:22 +0900

    change img path

--- src/components/Booth/booth.jsx
+++ src/components/Booth/booth.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import BoothImg from "../assets/booth_final.png";
+import BoothImg from "../../assets/booth_final.png";
 import { useNavigate } from "react-router-dom";
-import Location from "../assets/location.png";
+import Location from "../../assets/location.png";
 
 const Img = styled.div`
   float: right;
