0b2a8ff3b423ee49e0bdbfe4f8b3fb7f8a250511
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-05-11 20:21:17 +0900

    jsconfetti 추가

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import Banner from "../../assets/cuk_festival.png";
+import JSConfetti from "js-confetti";
 
 const BannerImg = () => {
+  useEffect(() => {
+    const jsConfetti = new JSConfetti({
+      startVelocity: 50, // Set initial velocity to a positive value
+      angle: 90, // Set angle to 90 degrees (straight up)
+      spread: 360, // Spread particles in all directions
+      elementCount: 50, // Number of particles to create
+    });
+    jsConfetti.addConfetti();
+  }, []);
   return (
     <div className="flex justify-center items-cente mt-10 h-5/6 opacity-70">
       <img
