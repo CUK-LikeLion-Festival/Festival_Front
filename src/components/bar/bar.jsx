945a5b229f6a1ffe09bed4d47a8a5b9100d280d7
OhHyukJun <ohj3961@gmail.com>
2023-05-16 18:29:08 +0900

    Feat: barmodal 창 추가 코드 작성

--- src/components/bar/bar.jsx
+++ src/components/bar/bar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Playground from "../img/playground.png";
+import Playground from "../../assets/playground.png";
 import styled from "styled-components";
 import BarList from "./barlist";
+
 
 const Container = styled.div`
   display: flex;
