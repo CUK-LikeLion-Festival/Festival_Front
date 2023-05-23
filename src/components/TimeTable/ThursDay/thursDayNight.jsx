043c1b4b6acd7cf339c662bdf464358725e89b9b
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-05-23 15:02:21 +0900

    timetable, modal, navbar 수정완료(se에 맞춤)

--- src/components/TimeTable/ThursDay/thursDayNight.jsx
+++ src/components/TimeTable/ThursDay/thursDayNight.jsx
@@ -18,26 +18,26 @@ export default function ThursDayNight() {
   return (
     <div>
       <div className="w-1/3 sm:w-full">
-        <h2 className="text-3xl text-center text-white font-bold sm:text-3xl">
+        <h2 className="text-3xl text-center text-white font-bold sm:text-2xl">
           5/25 Night
         </h2>
-        <hr className="border-white border-[1px] my-2 sm:mx-36 xl:mx-48" />
-        <p className="text-white text-center">축제 일정 안내</p>
+        <hr className="border-white border-[1px] my-2 sm:mx-36 sm:my-1 xl:mx-48" />
+        <p className="text-white text-center sm:text-sm">축제 일정 안내</p>
         <hr className="border-white border-[1px] my-4 sm:mx-7" />
         {clubPerformances.map((item, index) => (
           <div className="flex flex-col items-center mx-10 ">
-            <div className="text-white text-center mb-4 sm:mb-5 sm:mx-5 sm:p-2">
-              <h3 className="text-3xl font-bold">{item.title}</h3>
-              <p className="text-base">({item.time})</p>
+            <div className="text-white text-center mb-4 sm:mb-2 sm:mx-5 sm:p-2">
+              <h3 className="text-3xl font-bold sm:text-2xl">{item.title}</h3>
+              <p className="text-base sm:text-sm">({item.time})</p>
             </div>
             <div
               key={index}
-              className="bg-[#9C85AD] bg-opacity-60 border-[3px] border-white border-dashed  grid grid-cols-2 grid-flow-row gap-4 rounded-3xl p-4 mb-4 sm:h-auto sm:mb-5 sm:mx-5 sm:p-2 md:gap-7 md:grid-rows-2 md:grid-flow-row md:items-center"
+              className="bg-[#9C85AD] bg-opacity-60 border-[3px] border-white border-dashed  grid grid-cols-2 grid-flow-row gap-4 rounded-3xl p-4 mb-4 sm:h-auto sm:mb-2 sm:mx-5 sm:p-2 md:gap-7 md:grid-rows-2 md:grid-flow-row md:items-center"
             >
               {item.description.map((desc, idx) => (
                 <p
                   key={idx}
-                  className="text-white mb-2 text-xl  sm:text-[16px] sm:mb-1 md:text-base"
+                  className="text-white mb-2 text-xl   sm:text-[14px] sm:mb-1 md:text-base"
                 >
                   {desc ? `${idx + 1}. ${desc}` : desc}
                 </p>
@@ -48,18 +48,18 @@ export default function ThursDayNight() {
         <hr className="border-white border-[1px] my-2 sm:mx-7" />
         {influencer1.map((item, index) => (
           <div className="flex flex-col items-center mx-10 ">
-            <div className="text-white text-center mb-4 sm:mb-5 sm:mx-5 sm:p-2">
-              <h3 className="text-3xl font-bold">{item.title}</h3>
-              <p className="text-base">({item.time})</p>
+            <div className="text-white text-center mb-4 sm:mb-2 sm:mx-5 sm:p-2">
+              <h3 className="text-3xl font-bold sm:text-2xl">{item.title}</h3>
+              <p className="text-base sm:text-xs">({item.time})</p>
             </div>
             <div
               key={index}
-              className="bg-[#9C85AD] bg-opacity-60 border-[3px] border-white border-dashed  grid grid-cols-2 grid-flow-row gap-4 rounded-3xl p-4 mb-4 sm:h-auto sm:mb-5 sm:mx-5 sm:p-2 md:gap-7 md:grid-rows-2 md:grid-flow-row md:items-center"
+              className="bg-[#9C85AD] bg-opacity-60 border-[3px] border-white border-dashed  grid grid-cols-2 grid-flow-row gap-4 rounded-3xl p-4 mb-4 sm:h-auto sm:mb-2 sm:mx-5 sm:p-2 md:gap-7 md:grid-rows-2 md:grid-flow-row md:items-center"
             >
               {item.description.map((desc, idx) => (
                 <p
                   key={idx}
-                  className="text-white mb-2 text-xl  sm:text-[16px] sm:mb-1 md:text-base"
+                  className="text-white mb-2 text-xl  sm:text-[14px] sm:mx-2 sm:rounded-sm sm:mb-1 md:text-base"
                 >
                   {desc ? `${idx + 1}. ${desc}` : desc}
                 </p>
@@ -69,26 +69,26 @@ export default function ThursDayNight() {
         ))}
         <hr className="border-white border-[1px] my-2 sm:mx-7" />
         {gift && (
-          <div className="text-white text-center mb-4 sm:mb-5 sm:mx-5 sm:p-2">
-            <h3 className="text-3xl font-bold">{gift.title}</h3>
-            <p className="text-base">({gift.time})</p>
+          <div className="text-white text-center mb-4 sm:mb-2 sm:mx-5 sm:p-2">
+            <h3 className="text-3xl font-bold sm:text-2xl">{gift.title}</h3>
+            <p className="text-base sm:text-sm">({gift.time})</p>
           </div>
         )}
         <hr className="border-white border-[1px] my-2 sm:mx-7" />
         {influencer2.map((item, index) => (
           <div className="flex flex-col items-center mx-10 ">
-            <div className="text-white text-center mb-4 sm:mb-5 sm:mx-5 sm:p-2">
-              <h3 className="text-3xl font-bold">{item.title}</h3>
-              <p className="text-base">({item.time})</p>
+            <div className="text-white text-center mb-4 sm:mb-2 sm:mx-5 sm:p-2 ">
+              <h3 className="text-3xl font-bold sm:text-2xl">{item.title}</h3>
+              <p className="text-base sm:text-sm">({item.time})</p>
             </div>
             <div
               key={index}
-              className="bg-[#9C85AD] bg-opacity-60 border-[3px] border-white border-dashed  grid grid-cols-2 grid-flow-row gap-4 rounded-3xl p-4 mb-4 sm:h-auto sm:mb-5 sm:mx-5 sm:p-2 md:gap-7 md:grid-rows-2 md:grid-flow-row md:items-center"
+              className="bg-[#9C85AD] bg-opacity-60 border-[3px] border-white border-dashed  grid grid-cols-2 grid-flow-row gap-4 rounded-3xl p-4 mb-4 sm:h-auto sm:mb-2 sm:mx-5 sm:p-0  sm:rounded-3xl md:gap-7 md:grid-rows-2 md:grid-flow-row md:items-center"
             >
               {item.description.map((desc, idx) => (
                 <p
                   key={idx}
-                  className="text-white mb-2 text-xl  sm:text-[16px] sm:mb-1 md:text-base"
+                  className="text-white mb-2 text-xl  sm:text-[14px] sm:mb-1 sm:ml-2 md:text-base"
                 >
                   {desc ? `${idx + 1}. ${desc}` : desc}
                 </p>
--- src/components/TimeTable/WednesDay/wednesDayTimetable.jsx
+++ src/components/TimeTable/WednesDay/wednesDayTimetable.jsx
@@ -12,34 +12,34 @@ export default function WednesDayTimeTable() {
   return (
     <div>
       <div className="w-1/3 sm:w-full">
-        <h2 className="text-3xl text-center text-white font-bold sm:text-3xl">
+        <h2 className="text-3xl text-center text-white font-bold sm:text-2xl">
           5/24
         </h2>
         <hr className="border-white border-[1px] my-2 sm:mx-36 xl:mx-48" />
-        <p className="text-white text-center">축제 일정 안내</p>
-        <hr className="border-white border-[1px] my-4 sm:mx-7 " />
+        <p className="text-white text-center sm:text-sm">축제 일정 안내</p>
+        <hr className="border-white border-[1px] my-4 sm:mx-7 sm:my-1" />
 
         {opening && (
           <div className="text-white text-center mb-4 sm:mb-5 sm:mx-5 sm:p-2">
-            <h3 className="text-3xl font-bold">{opening.title}</h3>
-            <p className="text-base">({opening.time})</p>
+            <h3 className="text-3xl font-bold sm:text-2xl">{opening.title}</h3>
+            <p className="text-base sm:text-sm">({opening.time})</p>
           </div>
         )}
-        <hr className="border-white border-[1px] my-4 sm:mx-7" />
+        <hr className="border-white border-[1px] my-4 sm:mx-7 sm:my-0" />
         {clubPerformances.map((item, index) => (
           <div className="flex flex-col items-center mx-10 ">
-            <div className="text-white text-center mb-4 sm:mb-5 sm:mx-5 sm:p-2">
-              <h3 className="text-3xl font-bold">{item.title}</h3>
-              <p className="text-base">({item.time})</p>
+            <div className="text-white text-center mb-4 sm:mb-0 sm:mx-5 sm:p-2">
+              <h3 className="text-3xl font-bold sm:text-2xl">{item.title}</h3>
+              <p className="text-base sm:text-sm">({item.time})</p>
             </div>
             <div
               key={index}
-              className="bg-[#9C85AD] bg-opacity-60 border-[9px] border-white border-double  grid grid-cols-2 grid-flow-row gap-4 rounded-3xl p-4 mb-4 sm:h-auto sm:mb-5 sm:mx-5 sm:p-2 md:gap-7 md:grid-rows-2 md:grid-flow-row md:items-center"
+              className="bg-[#9C85AD] bg-opacity-60 border-[7px] border-white border-double  grid grid-cols-2 grid-flow-row gap-4 sm:gap-2 rounded-3xl p-4 mb-4 sm:h-auto sm:mb-5 sm:mx-5 sm:p-2 md:gap-7 md:grid-rows-2 md:grid-flow-row md:items-center"
             >
               {item.description.map((desc, idx) => (
                 <p
                   key={idx}
-                  className="text-white mb-2 text-xl  sm:text-[16px] sm:mb-1 md:text-base"
+                  className="text-white mb-2 text-xl  sm:text-[14px] sm:mb-0 md:text-base"
                 >
                   {desc ? `${idx + 1}. ${desc}` : desc}
                 </p>
@@ -47,18 +47,21 @@ export default function WednesDayTimeTable() {
             </div>
           </div>
         ))}
-        <hr className="border-white border-[1px] my-4 sm:mx-7" />
+        <hr className="border-white border-[1px] my-4 sm:mx-7 sm:my-0" />
         {closing && (
           <div className="text-white text-center mb-10  sm:mb-5 sm:mx-5 sm:p-2">
-            <h3 className="text-3xl font-bold">{closing.title}</h3>
-            <p p className="text-base">
+            <h3 className="text-3xl font-bold sm:text-2xl">{closing.title}</h3>
+            <p p className="text-base sm:text-sm">
               ({closing.time})
             </p>
           </div>
         )}
 
         <div className="flex justify-end items-center">
-          <p className="mr-2 text-2xl text-white">옆으로 슬라이드 해주세요</p>
+          <p className="mr-2 text-2xl text-white sm:text-xl">
+            옆으로 슬라이드 해주세요
+          </p>
+
           <FaArrowRight size={42} color="white" />
         </div>
       </div>
